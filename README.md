fcb640f0cd88f46fc0a508ad2bf76d9d9cdee1f1
rpvnwnkl <rpvnwnkl@gmail.com>
2015-11-19 22:19:21 -0500

    altered readme, updated autowrite, and deprecated wordcounter.py

--- README.md
+++ README.md
@@ -11,4 +11,13 @@ creates a text copy of all combined writing
 tells you what files are included
 tells you how many words total
 
+##Currently
+- This will run with 'python autoWrite.py' in Terminal
+- outputs number of words after quitting file
+
+##Does Not
+- write a file of all the words
+- Does not give overall word count
+- does not open text files of what you have written, for copying
+
 write one file, rewrites two new files and gives you word counts
--- autoWrite.py
+++ autoWrite.py
@@ -12,7 +12,7 @@ fileName = time.strftime('%d%a', rightNow)
 dirPath = time.strftime('%B%Y', rightNow)
 ##sets directory name based off date
 
-if dirPath not in os.listdir(os.getcwd()):
+if dirPath not in os.listdir('./'):
     ##creates new directory if the month hasn't been started yet
     os.mkdir(os.path.join(os.getcwd(), dirPath))
 
@@ -23,32 +23,51 @@ os.system('vim '+fileNamePath)
 ##sends out prompt to open vim with new filename in specified path
 ##creates a vim environment writing prompt within the terminal 
 
-todaysFile = open(fileNamePath, 'r')
-##opens file as todaysFile
-todaysLines = todaysFile.read()
-##reads file as string to todaysLines
-todaysString = todaysLines.replace('\n', ' ')
-##remove newlines
-todaysNoPunct = todaysString.translate(None, string.punctuation)
-todaysWords = todaysNoPunct.split(' ')
-##now we have a list of single words from the days writing
+def cleanFile(fileNamePath):
+##start er up with just the
+    todaysFile = open(fileNamePath, 'r')
+    ##opens file as todaysFile
+    todaysLines = todaysFile.read()
+    ##reads file as string to todaysLines
+    todaysString = todaysLines.replace('\n', ' ')
+    ##remove newlines
+    todaysNoPunct = todaysString.translate(None, string.punctuation)
+    todaysWords = todaysNoPunct.split(' ')
+    ##now we have a list of single words from the days writing
+    return todaysWords
 
-wordCount = 0
-realWords = []
-for word in todaysWords:
+def countWords(todaysWordList):
+    wordCount = 0
+    realWords = []
+    for word in todaysWordList:
     ##this will add up all the words which we think should qualify
-    if word not in ['', ' ', '  ', '   ']:
+        if word not in ['', ' ', '  ', '   ']:
         ##sifting out empty spaces and the like
-        wordCount += 1
-        realWords.append(word)
-        ##adds word to a cleaned up word list, sans punctuation and spaces
-    else:
-       pass
+            wordCount += 1
+            realWords.append(word)
+            ##adds word to a cleaned up word list, sans punctuation and spaces
+        else:
+            pass
+    return wordCount
 
-print str(wordCount)+' words for the day. \n So far.... '
+print 'OK, Here Is The Report:\n\n'
+print str(countWords(cleanFile(fileNamePath)))+' words for the day. \n So far.... \n\n'
+
+fileNames = []
+for fileEntry in os.listdir('./'+dirPath):
+    fileNames.append(fileEntry)
+fileNames.sort()
+##creates a list of the fileNames in the month's directory
+print 'Here are the files so far in the month of '+dirPath+': \
+        \n '+str(fileNames)+'\n'
     
 
-
+numWords = 0
+for name in fileNames:
+    wordsList = cleanFile('./'+dirPath+'/'+name)
+    numWords += countWords(wordsList)
+print 'and '+str(numWords)+' words for the month. \n So far.......\n'
+##Opens each file in fileNames list and counts the words thereh
 
 
 
